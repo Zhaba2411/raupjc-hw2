70217f6369360e0d47b3af41563ff344dc24152e
Janko Žabojec <jz49497@fer.hr>
2017-10-30 12:10:24 +0100

    Task1

---
+++ raupjc-hw2/Task1/Gender.cs
@@ -0,0 +1,7 @@
+﻿namespace Task1
+{
+    public enum Gender
+    {
+        Male, Female
+    }
+}
---
+++ raupjc-hw2/Task1/Student.cs
@@ -0,0 +1,38 @@
+﻿namespace Task1
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Jmbag { get; set; }
+        public Gender Gender { get; set; }
+
+        public Student(string name, string jmbag)
+        {
+            Name = name;
+            Jmbag = jmbag;
+        }
+
+        public override bool Equals(object obj)
+        {
+
+            if (!(obj is Student)) return false;
+            Student s = (Student) obj;
+            return Name.Equals(s.Name) && Jmbag.Equals(s.Jmbag);
+        }
+
+        public override int GetHashCode()
+        {
+            return Jmbag.GetHashCode() ^ Name.GetHashCode();
+        }
+
+        public static bool operator ==(Student obj1, Student obj2)
+        {
+            return (obj1.Name == obj2.Name && obj1.Jmbag == obj2.Jmbag);
+        }
+
+        public static bool operator !=(Student obj1, Student obj2)
+        {
+            return !(obj1.Name == obj2.Name && obj1.Jmbag == obj2.Jmbag);
+        }
+    }
+}
