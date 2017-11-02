5396faeeaaca8d2456d32bfbcebe6ab53a513402
Janko Žabojec <jz49497@fer.hr>
2017-11-02 13:23:21 +0100

    Task 5,6,7

---
+++ raupjc-hw2/Task5/readme.md
@@ -0,0 +1,10 @@
+﻿# Pitanje 1.:
+Izvodenje je trajalo 5 sekundi.
+# Pitanje 2.:
+Operacije su se izvodile na jednoj dretvi.
+# Pitanje 3.:
+Izvodenje je trajalo 1 sekundu.
+# Pitanje 4.:
+Operacije su se izvodile na 5 dretvi.
+# Pitanje 5.:
+U 
---
+++ raupjc-hw2/Task67/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task67
+{
+    public class Class1
+    {
+        public async Task<long> FactorialDigitSumAsync(int n)
+        {
+            long temp = 1, sum = 0;
+
+            while (n > 1)
+            {
+                temp = temp * n;
+                n--;
+            }
+
+            while (temp > 0)
+            {
+                sum = sum + temp % 10;
+                temp = temp / 10;
+            }
+
+            return sum;
+        } 
+    }
+}
